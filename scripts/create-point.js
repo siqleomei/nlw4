de74f6c3cfc451d852cdd602c93d539d943cf1b7
= <leonardo.mei@chiptronic.com.br>
2021-03-31 17:41:40 -0300

    Conclusão da aula 3, codificação da interface de buscas e resultado de pesquisa, e a utilização de modal

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -16,10 +16,11 @@ function getCities(event){
 
     const citySelect = document.querySelector("select[name=city]")
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${event.target.value}/municipios`
+    citySelect.disabled = true
     fetchValues(url, cities => {
         citySelect.innerHTML = '<option value="">Selecione a cidade</option>'
         for (city of cities){
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
         citySelect.disabled = false
     })
@@ -32,4 +33,45 @@ function fetchValues(url, func){
 }
 
 document.querySelector("select[name=uf]")
-.addEventListener("change", getCities)
+        .addEventListener("change", getCities);
+
+ // Itens de coleta
+ const itemsToCollect = document.querySelectorAll(".itens-grid li");
+ const collectedItems = document.querySelector("input[name=items]");
+
+ for (let item of itemsToCollect){
+     item.addEventListener("click", handleSelectedItem);
+ }
+
+let selectedItems = [];
+
+ function handleSelectedItem(event){
+    const itemLi = event.target;
+
+    // Adicionar ou remover uma classe com javascript
+    //.add = Adiciona
+    //.remove = Remove
+    //.toggle = Adiciona ou remove, dependendo do necessário
+    itemLi.classList.toggle("selected");
+
+    const itemId = itemLi.dataset.id;
+
+    const alreadySelected = selectedItems.findIndex((item)=>{return item == itemId});
+    if (alreadySelected >= 0){
+        console.log("Ja estava clicado");
+
+        //Tirar da seleção, percorrerá o array e removerá o item q for diferente
+        const filteredItens = selectedItems.filter(item=>{
+            const itemIsDifferent = item != itemId;
+            return itemIsDifferent;
+        });
+
+        selectedItems = filteredItens;
+
+    } else {
+        console.log("Não estava clicado");
+        selectedItems.push(itemId);
+    }
+
+    collectedItems.value = selectedItems;
+ }
