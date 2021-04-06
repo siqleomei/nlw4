a48ad44e32b02268da5e5baf86e4287eacfb282a
siqleomei <leonardo.mei@chiptronic.com.br>
2021-04-06 09:21:27 -0300

    Alterando projeto para Node.js e iniciando a utilizando de nunjucks

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const server = express();
+
+// configurar pasta publica
+server.use(express.static("public"));
+
+// Utilizando template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+// configurar caminhos na minha aplicação
+// pagina inicial
+// req: Requisição - res: Resposta
+server.get("/", (req, res)=>{
+    return res.render("index.html");
+});
+
+server.get("/create-point", (req, res)=>{
+    return res.render("create-point.html");
+});
+
+server.get("/search-results", (req, res)=>{
+    return res.render("search-results.html");
+});
+
+// ligar o servidor
+server.listen(3000)
